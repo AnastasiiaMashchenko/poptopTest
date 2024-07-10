d03ec495742cb5a3eb2f28b894bbe160ff94c599
nana <nana@poptop.co.uk>
2024-07-10 21:11:26 +0100

    new tests added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("/auth");
-
-  //   // Expect a title "to contain" a substring.
+  await page.goto("/");
   await expect(page).toHaveTitle(/Poptop Parties UK - Party experiences for extraordinary private and corporate events/);
+  const expectedMainText = "Home of extraordinary party experiences";
+  expect(await page.getByTestId("HeroSection").locator("h1").innerText()).toEqual(expectedMainText);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
+test("navigatre to search via event form", async ({ page }) => {
+  await page.goto("/");
+  await page.getByTestId("Button").getByText("SEARCH").click();
+  await page.waitForURL("**/search/?");
+  await expect(page.locator("div[data-scrollbar]"), "Search page didn't opened").toBeVisible({ timeout: 10000 });
+});
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
+test("has banner view now", async ({ page }) => {
+  await page.goto("/");
+  await page.getByTestId("Button").getByText("VIEW NOW").click();
+  await page.waitForURL("/services/29981/");
+});
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
-// });
+test("", async ({ page }) => {
+
+})
